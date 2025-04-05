d000678be31b04a52e61c2b4d00758c64a06b654
Saoma0 <144680065+Saoma0@users.noreply.github.com>
2025-04-05 02:01:43 +0200

    Initial commit

---
+++ BodyBalanceNow/Services/ExerciseDatabase.cs
@@ -0,0 +1,26 @@
+﻿
+
+using BodyBalanceNow.Models;
+using System.Diagnostics;
+
+namespace BodyBalanceNow.Services
+{
+    public class ExerciseDatabase
+    {
+        private ConfigConnection config;
+        private string connectionString;
+
+        // Constructor para inicializar los valores
+        public ExerciseDatabase()
+        {
+            config = ConfigConnection.CargarDesdeJson(); // Carga la configuración
+            connectionString = config.GetConnectionString(); // Genera el string de conexión
+        }
+
+        // Ejemplo de método para usar la conexión
+        public void MostrarConnectionString()
+        {
+            Debug.WriteLine($"Conexión lista con: {connectionString}");
+        }
+    }
+}
